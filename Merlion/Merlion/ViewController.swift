af52608c3fe01fa5c6b111553da59cd901818bcc
Kageken <kageken@KagekennoMacBook-Pro.local>
2019-06-06 20:45:04 +0900

    Create project files

---
+++ Merlion/Merlion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Merlion
+//
+//  Created by 陰山賢太 on 2019/06/06.
+//  Copyright © 2019 Kageken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
