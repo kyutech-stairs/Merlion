842ae9126757b9ecb349f7b829e65cfbe6922323
Kageken <kageken@KagekennoMacBook-Pro.local>
2019-06-08 17:49:20 +0900

    Modified set to notify time

--- Merlion/Merlion/SecondViewController.swift
+++ Merlion/Merlion/SecondViewController.swift
@@ -11,6 +11,8 @@ import UserNotifications
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var picker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,12 +24,21 @@ class SecondViewController: UIViewController {
 
         print("notification will be triggered in five seconds..Hold on tight")
         let content = UNMutableNotificationContent()
-        content.title = "Intro to Notifications"
-        content.subtitle = "Lets code,Talk is cheap"
-        content.body = "Sample code from WWDC"
+        content.title = "Title"
+        content.subtitle = "subtitle"
+        content.body = "message"
         content.sound = UNNotificationSound.default
 
-        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 5.0, repeats: false)
+        var fireDate = DateComponents()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH"
+        fireDate.hour = Int(formatter.string(from: picker.date))
+        formatter.dateFormat = "mm"
+        fireDate.minute = Int(formatter.string(from: picker.date))
+        fireDate.second = 0
+
+        //let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 10.0, repeats: false)
+        let trigger = UNCalendarNotificationTrigger.init(dateMatching: fireDate, repeats: false)
         let request = UNNotificationRequest(identifier:"notificationid", content: content, trigger: trigger)
 
         UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
